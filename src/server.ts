8452711a579206d776c91a3cf133b3072e9c2ae5
touhidcodes <touhidcodes@gmail.com>
2024-10-29 20:00:17 +0600

    feat: prisma binary support

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import { Server } from "http";
 import app from "./app";
 import config from "./app/config/config";
-import { seedTestCredentials } from "./app/utils/seedTestCredentials";
 import { seedSuperAdmin } from "../prisma/seed";
 
 async function main() {
   seedSuperAdmin();
-  seedTestCredentials();
+
   const server: Server = app.listen(config.port, () => {
     console.log("Sever is running on port ", config.port);
   });
