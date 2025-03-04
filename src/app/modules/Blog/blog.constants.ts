d5ea25e9f0749af74fb0318ff3ad2c3e8fa8fc9d
touhidcodes <touhidcodes@gmail.com>
2025-03-04 23:29:44 +0600

    add: blog constants

---
+++ src/app/modules/Blog/blog.constants.ts
@@ -0,0 +1,9 @@
+// for all searching
+export const blogSearchableFields: string[] = ["title", "category"];
+
+// for all filtering
+export const blogFilterableFields: string[] = [
+  "category",
+  "featured",
+  "searchTerm",
+];
