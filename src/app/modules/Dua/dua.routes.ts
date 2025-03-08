8f1689a016d87a21f13c0b257db8ea071feb6fb8
touhidcodes <touhidcodes@gmail.com>
2025-03-08 21:32:04 +0600

    add: dua routes

---
+++ src/app/modules/Dua/dua.routes.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import { blogValidationSchemas } from "./blog.validation";
+import { blogControllers } from "./blog.controller";
+import validateRequest from "../../middlewares/validateRequest";
+import auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
+
+const router = express.Router();
+
+router.get("/all", blogControllers.getBlogs);
+
+router.post(
+  "/",
+  auth(UserRole.SUPERADMIN, UserRole.ADMIN),
+  validateRequest(blogValidationSchemas.createBlogSchema),
+  blogControllers.createBlog
+);
+
+router.get("/:blogId", blogControllers.getBlogById);
+
+router.put(
+  "/:blogId",
+  auth(UserRole.SUPERADMIN, UserRole.ADMIN),
+  validateRequest(blogValidationSchemas.updateBlogSchema),
+  blogControllers.updateBlog
+);
+
+router.delete(
+  "/:blogId",
+  auth(UserRole.SUPERADMIN, UserRole.ADMIN),
+  blogControllers.deleteBlog
+);
+
+export const duaRoutes = router;
