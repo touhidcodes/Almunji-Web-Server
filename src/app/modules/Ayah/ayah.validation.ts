df75524949a586d93037c8d0449541b380dc2150
touhidcodes <touhidcodes@gmail.com>
2025-03-21 22:47:48 +0600

    add: ayah validation

---
+++ src/app/modules/Ayah/ayah.validation.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+// Schema to create an Ayah
+const createAyahSchema = z.object({
+  body: z.object({
+    surahId: z.string({ required_error: "Surah ID is required" }),
+    ayahNumber: z
+      .number({ required_error: "Ayah number is required" })
+      .int()
+      .positive(),
+    arabicText: z.string({ required_error: "Arabic text is required" }),
+    banglaText: z.string().optional(),
+    englishText: z.string().optional(),
+  }),
+});
+
+// Schema to update an Ayah
+const updateAyahSchema = z.object({
+  body: z.object({
+    surahId: z.string().optional(),
+    ayahNumber: z.number().int().positive().optional(),
+    arabicText: z.string().optional(),
+    banglaText: z.string().optional(),
+    englishText: z.string().optional(),
+  }),
+});
+
+export const ayahValidationSchema = {
+  createAyahSchema,
+  updateAyahSchema,
+};
