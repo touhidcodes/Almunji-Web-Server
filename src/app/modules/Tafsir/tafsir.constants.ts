5022c9f1a607e5c860832b8a91186cd25f47315e
touhidcodes <touhidcodes@gmail.com>
2025-04-20 15:45:06 +0600

    add: tafsir constants

---
+++ src/app/modules/Tafsir/tafsir.constants.ts
@@ -0,0 +1,8 @@
+// For filter
+export const tafsirFilterableFields: string[] = ["searchTerm"];
+
+// For pagination
+export const tafsirPaginationFields: string[] = ["limit", "page"];
+
+// For query
+export const tafsirQueryFields: string[] = ["title", "text", "scholar"];
