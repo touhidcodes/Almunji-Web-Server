509d583d22c0271603cd85ebc7a882a26073992a
touhidcodes <touhidcodes@gmail.com>
2025-04-26 11:47:13 +0600

    add: dua controllers

---
+++ src/app/modules/Dua/dua.constants.ts
@@ -0,0 +1,19 @@
+// For filter
+export const duaFilterableFields: string[] = [
+  "searchTerm",
+  "isDeleted",
+  "tags",
+  "sortBy",
+  "sortOrder",
+];
+
+// For pagination
+export const duaPaginationFields: string[] = ["limit", "page"];
+
+// For query
+export const duaQueryFields: string[] = [
+  "arabic",
+  "bangla",
+  "english",
+  "reference",
+];
