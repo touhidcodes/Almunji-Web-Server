2ea33f21d5cbd57e98994b953847f52812b0cb42
touhidcodes <touhidcodes@gmail.com>
2025-04-26 11:58:57 +0600

    fix: dua validation

--- src/app/modules/Dua/dua.validation.ts
+++ src/app/modules/Dua/dua.validation.ts
@@ -1,22 +1,28 @@
 import { z } from "zod";
 
+// Validation for creating a new Dua
 const createDuaSchema = z.object({
   body: z.object({
     name: z.string().min(1, "Name is required"),
-    arabicText: z.string().min(1, "Arabic text is required"),
+    arabic: z.string().min(1, "Arabic text is required"),
     transliteration: z.string().optional(),
-    bangla: z.string().min(1, "Bangla Text is required"),
+    bangla: z.string().min(1, "Bangla text is required"),
+    english: z.string().optional(),
     reference: z.string().optional(),
+    tags: z.string().optional(),
   }),
 });
 
+// Validation for updating an existing Dua
 const updateDuaSchema = z.object({
   body: z.object({
     name: z.string().optional(),
     arabic: z.string().optional(),
     transliteration: z.string().optional(),
     bangla: z.string().optional(),
+    english: z.string().optional(),
     reference: z.string().optional(),
+    tags: z.string().optional(),
   }),
 });
 
