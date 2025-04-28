521ec60d6de90856051f18c2484fef731724bab9
touhidcodes <touhidcodes@gmail.com>
2025-04-28 21:33:17 +0600

    fix: blog validation

--- src/app/modules/Blog/blog.validation.ts
+++ src/app/modules/Blog/blog.validation.ts
@@ -4,8 +4,8 @@ const createBlogSchema = z.object({
   body: z.object({
     title: z.string().min(1, "Title is required"),
     thumbnail: z.string().optional(),
+    summary: z.string().optional(),
     content: z.string().min(1, "Content is required"),
-    categoryId: z.string().min(1, "Category ID is required"),
   }),
 });
 
@@ -13,9 +13,10 @@ const updateBlogSchema = z.object({
   body: z.object({
     title: z.string().optional(),
     thumbnail: z.string().optional(),
+    summary: z.string().optional(),
     content: z.string().optional(),
-    categoryId: z.string().optional(),
-    published: z.boolean().optional(),
+    isPublished: z.boolean().optional(),
+    IsFeatured: z.boolean().optional(),
   }),
 });
 
