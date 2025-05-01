e3a74d5efb1353fc7900c1fd199e062300212ebf
touhidcodes <touhidcodes@gmail.com>
2025-05-01 18:53:07 +0600

    add: book content controller

---
+++ src/app/modules/BookContent/bookContent.controller.ts
@@ -0,0 +1,97 @@
+import httpStatus from "http-status";
+import sendResponse from "../../utils/sendResponse";
+import catchAsync from "../../utils/catchAsync";
+import queryPickers from "../../utils/queryPickers";
+
+// Create a Book Content
+const createBookContent = catchAsync(async (req, res) => {
+  const result = await bookContentServices.createBookContent(req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Book content added successfully!",
+    data: result,
+  });
+});
+
+// Get all Book Contents
+const getAllBookContent = catchAsync(async (req, res) => {
+  const filters = queryPickers(req.query, bookContentFilterableFields);
+  const pagination = queryPickers(req.query, bookContentPaginationFields);
+  const result = await bookContentServices.getAllBookContents(
+    filters,
+    pagination
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Book contents retrieved successfully!",
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
+// Get a single Book Content by ID
+const getBookContentById = catchAsync(async (req, res) => {
+  const { contentId } = req.params;
+  const result = await bookContentServices.getBookContentById(contentId);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Book content retrieved successfully!",
+    data: result,
+  });
+});
+
+// Update Book Content
+const updateBookContent = catchAsync(async (req, res) => {
+  const { contentId } = req.params;
+  const result = await bookContentServices.updateBookContent(
+    contentId,
+    req.body
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Book content updated successfully!",
+    data: result,
+  });
+});
+
+// Soft Delete Book Content
+const deleteBookContent = catchAsync(async (req, res) => {
+  const { contentId } = req.params;
+  const result = await bookContentServices.deleteBookContent(contentId);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Book content soft-deleted successfully!",
+    data: result,
+  });
+});
+
+// Hard Delete Book Content (Admin only)
+const deleteBookContentByAdmin = catchAsync(async (req, res) => {
+  const { contentId } = req.params;
+  const result = await bookContentServices.deleteBookContentByAdmin(contentId);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Book content hard-deleted successfully!",
+    data: result,
+  });
+});
+
+export const bookContentControllers = {
+  createBookContent,
+  getAllBookContent,
+  getBookContentById,
+  updateBookContent,
+  deleteBookContent,
+  deleteBookContentByAdmin,
+};
