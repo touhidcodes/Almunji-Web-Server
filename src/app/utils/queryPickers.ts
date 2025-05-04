3971ca8ab1cd1fc36220e8a2c4338976a84a1a3f
touhidcodes <touhidcodes@gmail.com>
2025-05-04 23:42:46 +0600

    add: new query picker func

--- src/app/utils/queryPickers.ts
+++ src/app/utils/queryPickers.ts
@@ -15,3 +15,33 @@ const queryPickers = <T extends Record<string, unknown>, k extends keyof T>(
 };
 
 export default queryPickers;
+
+export function extractFilters<
+  T extends Record<string, any>,
+  K extends keyof T,
+  P extends keyof T
+>(
+  query: T,
+  knownKeys: readonly K[],
+  paginationKeys: readonly P[]
+): {
+  filters: Pick<T, K>;
+  pagination: Pick<T, P>;
+  additional: Omit<T, K | P>;
+} {
+  const filters = {} as Pick<T, K>;
+  const pagination = {} as Pick<T, P>;
+  const additional = {} as Omit<T, K | P>;
+
+  Object.keys(query).forEach((key) => {
+    if (paginationKeys.includes(key as any)) {
+      (pagination as any)[key] = query[key];
+    } else if (knownKeys.includes(key as any)) {
+      (filters as any)[key] = query[key];
+    } else {
+      (additional as any)[key] = query[key];
+    }
+  });
+
+  return { filters, pagination, additional };
+}
