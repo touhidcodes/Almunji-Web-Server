99d9d21bc6ccfb403c6bd786e2307a3fac7df02c
Touhidur Zaman <touhidcodes@gmail.com>
2026-02-12 23:03:03 +0600

    mod: auth controller

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -3,31 +3,33 @@ import catchAsync from "../../utils/catchAsync";
 import sendResponse from "../../utils/sendResponse";
 import { authServices } from "./auth.service";
 
-// Controller to create user
+// Create User
+
 const createUser = catchAsync(async (req, res) => {
   const result = await authServices.createUser(req.body);
+
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: "User registered successfully!",
     data: {
-      id: result.createdUserData.id,
-      username: result.createdUserData.username,
-      email: result.createdUserData.email,
+      id: result.user.id,
+      username: result.user.username,
+      email: result.user.email,
       token: result.accessToken,
     },
   });
 });
 
-// Controller to login user
+// Login User
+
 const loginUser = catchAsync(async (req, res) => {
   const result = await authServices.loginUser(req.body);
 
-  const { refreshToken } = result;
-
-  res.cookie("refreshToken", refreshToken, {
-    secure: false,
+  res.cookie("refreshToken", result.refreshToken, {
     httpOnly: true,
+    secure: false,
+    sameSite: "strict",
   });
 
   sendResponse(res, {
@@ -35,17 +37,22 @@ const loginUser = catchAsync(async (req, res) => {
     success: true,
     message: "Logged in successfully!",
     data: {
-      id: result.userData.id,
-      username: result.userData.username,
-      email: result.userData.email,
+      id: result.user.id,
+      username: result.user.username,
+      email: result.user.email,
       token: result.accessToken,
     },
   });
 });
 
-// Controller to get refresh token
+// Refresh Token
+
 const refreshToken = catchAsync(async (req, res) => {
   const { refreshToken } = req.cookies;
+
+  if (!refreshToken) {
+    throw new Error("Refresh token not found");
+  }
 
   const result = await authServices.refreshToken(refreshToken);
 
@@ -53,25 +60,24 @@ const refreshToken = catchAsync(async (req, res) => {
     statusCode: httpStatus.OK,
     success: true,
     message: "Access token generated successfully!",
-    data: { token: result.accessToken },
+    data: {
+      token: result.accessToken,
+    },
   });
 });
 
-// Controller to change password
+// Change Password
 const changePassword = catchAsync(async (req, res) => {
-  const { userId } = req.user;
-  const { ...passwordData } = req.body;
+  const { id: userId } = req.user;
+  const passwordData = req.body;
 
   await authServices.changePassword(userId, passwordData);
 
   sendResponse(res, {
-    statusCode: 200,
+    statusCode: httpStatus.OK,
     success: true,
     message: "Password changed successfully!",
-    data: {
-      status: 200,
-      message: "Password changed successfully!",
-    },
+    data: null,
   });
 });
 
